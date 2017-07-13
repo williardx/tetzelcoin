210fc5433cf718842380be2b9e28bebbb7716fc3
Will Doenlen <wdoenlen@gmail.com>
2017-07-12 17:41:54 -0700

    Contracts correctly instantiate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import Tetzel from '../build/contracts/Tetzel.json'
+import TetzelCrowdsale from '../build/contracts/TetzelCrowdsale.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -13,29 +14,25 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      tetzelInstance: null,
+      tetzelCoinAddress: null,
     }
   }
 
-  componentWillMount() {
+  async componentWillMount() {
     // Get network provider and web3 instance.
     // See utils/getWeb3 for more info.
-
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
-
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
+    try {
+      var web3 = await getWeb3;
+      this.setState({web3: web3.web3});
+      await this.instantiateContracts();
+    } catch(e) {
+      console.log(e);
+    }
   }
 
-  instantiateContract() {
+  async instantiateContracts() {
     /*
      * SMART CONTRACT EXAMPLE
      *
@@ -44,27 +41,21 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const tetzel = contract(Tetzel)
+    const tetzelCrowdsale = contract(TetzelCrowdsale)
+    
+    tetzel.setProvider(this.state.web3.currentProvider)
+    tetzelCrowdsale.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var tetzelInstance = await tetzel.deployed();
+    var tetzelCrowdsaleAddress = await tetzelInstance.crowdsale.call();
+    var tetzelCrowdsaleInstance = await tetzelCrowdsale.at(tetzelCrowdsaleAddress);
+    var tetzelCoinAddress = await tetzelCrowdsaleInstance.token.call();
 
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
+    this.setState({
+      tetzelInstance: tetzelInstance,
+      tetzelCoinAddress: tetzelCoinAddress
+    });
   }
 
   render() {
