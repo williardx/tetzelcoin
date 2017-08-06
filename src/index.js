b7888b64af35a9625201216b31a90c2f4eee6f3c
Will Doenlen <wdoenlen@gmail.com>
2017-08-05 18:09:44 -0700

    Add react router and create new component for marketing site

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <HashRouter>
+    <App />
+  </HashRouter>
+  ), document.getElementById('root')
 );
