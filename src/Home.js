5c8892d300585f8159148c66acff1387a9ac176a
Will Doenlen <wdoenlen@gmail.com>
2017-08-05 18:54:54 -0700

    Template for marketing site

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,145 @@
-import React from 'react';
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Visibility,
+} from 'semantic-ui-react'
 
-const Home = () => (
-  <div>
-    <h1>Hello world!</h1>
-  </div>
-);
+export default class Home extends Component {
 
-export default Home;
+  render() {
+
+    return (
+      <div>
+        <Segment
+          inverted
+          textAlign='center'
+          style={{ minHeight: 700, padding: '1em 0em' }}
+          vertical
+        >
+          <Container text>
+            <Header
+              as='h1'
+              content='TetzelCoin'
+              inverted
+              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+            />
+            <Header
+              as='h2'
+              content='A token for forgiveness'
+              inverted
+              style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+            />
+            <Button primary size='huge'>
+              Whitepaper
+            </Button>
+          </Container>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as='h3' style={{ fontSize: '2em' }}>What is TetzelCoin?</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  TetzelCoin is a cryptocurrency designed to be a token for forgiveness. Since we cannot forgive and forget, users are able to define their sins in monetary terms and take appropriate action to correct their misdeeds.
+                </p>
+              </Grid.Column>
+              <Grid.Column floated='right' width={6}>
+                <Image
+                  bordered
+                  rounded
+                  size='large'
+                  src='/assets/images/wireframe/white-image.png'
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Container text>
+            <Header as='h3' style={{ fontSize: '2em' }}>Confession Begins</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Instead of focusing on content creation and hard work, we have learned how to master the art of doing
+              nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
+              and worth your attention.
+            </p>
+          </Container>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as='h3' style={{ fontSize: '2em' }}>How does it work?</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  TODO: How it works description
+                </p>
+              </Grid.Column>
+              <Grid.Column floated='right' width={6}>
+                <Image
+                  bordered
+                  rounded
+                  size='large'
+                  src='/assets/images/wireframe/white-image.png'
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign='middle'>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as='h3' style={{ fontSize: '2em' }}>Token Sale</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  The TetzelCoin token sale will begin on October 31st at 12PM PST (the 500th Anniversary of Reformation Day) and will end January 1st, 2018, 12 AM PST.
+                </p>
+                <p style={{ fontSize: '1.33em' }}>
+                  The token sale is uncapped and there is an unlimited supply of tokens.  Tokens will be available immediately upon confession. Only ether will be accepted as payment.
+                </p>
+                <p style={{ fontSize: '1.33em' }}>
+                  90% of the proceeds from the token sale will be donated directly to RIP Medical Debt. The remaining 10% will go towards covering operating costs and compensating the team.
+                </p>
+                <p style={{ fontSize: '1.33em' }}>
+                  View token sale terms here.
+                </p>
+              </Grid.Column>
+              <Grid.Column floated='right' width={6}>
+                <Image
+                  bordered
+                  rounded
+                  size='large'
+                  src='/assets/images/wireframe/white-image.png'
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={16}>
+                  <Header as='h4' inverted>Footer Header</Header>
+                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+      </div>
+    )
+  }
+}
