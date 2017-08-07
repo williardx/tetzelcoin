03384b113a94049c3ffa91d2c76e87ad55c8f7b1
Will Doenlen <wdoenlen@gmail.com>
2017-08-06 22:04:17 -0700

    More CSS munging

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,6 @@ import CountdownTimer from './components/CountdownTimer';
 import './css/dswallau.css';
 import './css/home.css';
 
-
 export default class Home extends Component {
 
   render() {
@@ -50,7 +49,7 @@ export default class Home extends Component {
             />
             <Header
               as='h4'
-              content='Sign up to receive updates about TetzelCoin and token sale '
+              content='Sign up to receive updates about TetzelCoin and the token sale'
               style={{ fontSize: '1.2em', fontWeight: 'normal' }}
             />
             <Input
@@ -84,42 +83,44 @@ export default class Home extends Component {
         </Segment>
 
         <Segment style={{ padding: '8em 0em' }} vertical>
-          <Grid container stackable verticalAlign='middle'>
-            <Grid.Row>
-              <Header as='h3' style={{ fontSize: '2em' }}>How does it work?</Header>              
-            </Grid.Row>
-            <Grid.Row>
-              <Image
-                fluid
-                style={{height: '100%'}}
-                src='/images/TetzelCoin_How-It-Works.png'
-              />
-            </Grid.Row>
-            <Grid.Row>
-              <Header as='h4' style={{ fontSize: '1.5em' }}>Confession</Header>                            
-            </Grid.Row>
-            <Grid.Row>
-              <Image
-                fluid
-                size='huge'
-                style={{height: '100%'}}
-                src='/images/TetzelCoin_MedicalDebt.png'
-              />
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as='h4' style={{ fontSize: '1.5em' }}>Forgiveness</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  TetzelCoin is a cryptocurrency designed to be a token for forgiveness. Since we cannot forgive and forget, users are able to define their sins in monetary terms and take appropriate action to correct their misdeeds. (Edit text later...)
-                </p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
+          <Container>
+            <Grid stackable verticalAlign='middle'>
+              <Grid.Row>
+                <Header as='h3' style={{ fontSize: '2em' }}>How does it work?</Header>              
+              </Grid.Row>
+              <Grid.Row>
                 <Image
-                  src='/images/TetzelCoin_Forgiveness.png'
+                  fluid
+                  style={{height: '100%'}}
+                  src='/images/TetzelCoin_How-It-Works.png'
                 />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+              </Grid.Row>
+              <Grid.Row>
+                <Header as='h4' style={{ fontSize: '1.5em' }}>Confession</Header>                            
+              </Grid.Row>
+              <Grid.Row>
+                <Image
+                  fluid
+                  size='huge'
+                  style={{height: '100%'}}
+                  src='/images/TetzelCoin_MedicalDebt.png'
+                />
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Header as='h4' style={{ fontSize: '1.5em' }}>Forgiveness</Header>
+                  <p style={{ fontSize: '1.33em' }}>
+                    TetzelCoin is a cryptocurrency designed to be a token for forgiveness. Since we cannot forgive and forget, users are able to define their sins in monetary terms and take appropriate action to correct their misdeeds. (Edit text later...)
+                  </p>
+                </Grid.Column>
+                <Grid.Column floated='right' width={6}>
+                  <Image
+                    src='/images/TetzelCoin_Forgiveness.png'
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
         </Segment>
 
         <Segment className='dark-gray-background' style={{ padding: '8em 0em' }} vertical>
@@ -133,48 +134,64 @@ export default class Home extends Component {
         </Segment>
 
         <Segment style={{ padding: '8em 0em' }} vertical>
-          <Grid container stackable verticalAlign='middle'>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as='h3' style={{ fontSize: '2em' }}>Token Sale</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  The TetzelCoin token sale will begin on October 31st at 12PM PST (the 500th Anniversary of Reformation Day) and will end January 1st, 2018, 12 AM PST.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  The token sale is uncapped and there is an unlimited supply of tokens.  Tokens will be available immediately upon confession. Only ether will be accepted as payment.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  90% of the proceeds from the token sale will be donated directly to RIP Medical Debt. The remaining 10% will go towards covering operating costs and compensating the team.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  View token sale terms here.
-                </p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
-                <Image
-                  bordered
-                  rounded
-                  size='large'
-                  src='/assets/images/wireframe/white-image.png'
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <Container>
+            <Grid stackable verticalAlign='middle'>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Header as='h3' style={{ fontSize: '2em' }}>Token Sale</Header>
+                  <p style={{ fontSize: '1.33em' }}>
+                    The TetzelCoin token sale will begin on October 31st at 12PM PST (the 500th Anniversary of Reformation Day) and will end January 1st, 2018, 12 AM PST.
+                  </p>
+                  <p style={{ fontSize: '1.33em' }}>
+                    The token sale is uncapped and there is an unlimited supply of tokens.  Tokens will be available immediately upon confession. Only ether will be accepted as payment.
+                  </p>
+                  <p style={{ fontSize: '1.33em' }}>
+                    90% of the proceeds from the token sale will be donated directly to RIP Medical Debt. The remaining 10% will go towards covering operating costs and compensating the team.
+                  </p>
+                  <p style={{ fontSize: '1.33em' }}>
+                    View token sale terms here.
+                  </p>
+                </Grid.Column>
+                <Grid.Column floated='right' width={6}>
+                  <Image
+                    bordered
+                    rounded
+                    size='large'
+                    src='/assets/images/wireframe/white-image.png'
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
         </Segment>
 
         <Segment style={{ padding: '8em 0em' }} vertical className='dark-gray-background'>
-          <Grid container stackable verticalAlign='middle'>
-            <Grid.Row>
-              <Header as='h3' style={{ fontSize: '2em' }}>Team</Header>            
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-              </Grid.Column>
-              <Grid.Column>
-              </Grid.Column>
-            </Grid.Row>
+          <Container>
+            <Grid stackable verticalAlign='middle'>
+              <Grid.Row>
+                <Grid.Column textAlign='center'>
+                  <Header as='h3' style={{ fontSize: '2em' }}>Team</Header>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                </Grid.Column>
+                <Grid.Column>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>          
+          </Container>
+        </Segment>
 
-          </Grid>
+        <Segment textAlign='center' style={{ padding: '8em 0em' }} vertical>
+          <Container>
+            <Header as='h3' style={{ fontSize: '2em' }}>Receive updates about TetzelCoin and the token sale</Header>                      
+            <Input
+              className='email-input'
+              action={<Button primary size='big' className='btn-cta'>Sign Up</Button>}
+              placeholder='your@email.com'
+            />
+          </Container>
         </Segment>
 
         <Segment vertical style={{ padding: '5em 0em' }}>
