9208b0e868e781c34bcabf91facfe83e67d3cf72
Will Doenlen <wdoenlen@gmail.com>
2017-08-16 17:47:30 -0700

    Begin integrating threejs confessional scene into react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Confess from './Confess';
 import Home from './Home';
+import ConfessionalTest from './ConfessionalTest';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route path="/confess" component={ Confess } />
+      <Route path="/confessionaltest" component = { ConfessionalTest } />
     </Switch>
   </main>
 );
---
+++ src/ConfessionalTest.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ConfessionalScene from './components/ConfessionalScene';
+
+export default class ConfessionalTest extends Component {
+
+  render() {
+    return(
+      <div>
+        <ConfessionalScene />
+      </div>
+    );
+  }
+
+}
