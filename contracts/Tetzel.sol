4875d2a690b4fbb3c7d4658a93c57e02bfc466f3
Will Doenlen <wdoenlen@gmail.com>
2017-10-02 13:35:34 -0700

    Slim down main Tetzel contract to reduce need for storage - remove sins map, etc

--- contracts/Tetzel.sol
+++ contracts/Tetzel.sol
@@ -1,20 +1,14 @@
 pragma solidity ^0.4.11;
 
 import './TetzelCrowdsale.sol';
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 contract Tetzel {
 
   address owner; 
   TetzelCrowdsale public crowdsale;
 
-  // Registry of all sins allowing anyone to look up the SHA256 hashes of
-  // a user's confessions
-  mapping (address => bytes32[]) public sins;
-
   event Confess(
     address indexed sinner,
-    bytes32 indexed sinHash,
     uint256 indexed payment,
     string sin
   );
@@ -42,30 +36,13 @@ contract Tetzel {
       throw;
     }
 
-    // Register the sin and store the hash of the sin in the sins mapping
-    address sinner = msg.sender;
-    bytes32 sinHash = getSinHash(sin);
-    sins[sinner].push(sinHash);
-
     // Issue TetzelCoin for the sinner
     crowdsale.buyTokens.value(msg.value)(msg.sender); 
 
     // Make the confession known!
-    Confess(sinner, sinHash, msg.value, sin);
+    Confess(msg.sender, msg.value, sin);
 
     return true;
-  }
-
-  function getSins() constant returns (bytes32[]) {
-    return sins[msg.sender];
-  }
-
-  function getSinsByAddress(address addr) constant returns (bytes32[]) {
-    return sins[addr];
-  }
-
-  function getSinHash(string sin) constant returns (bytes32) {
-    return sha256(sin);
   }
 
   function destroy() onlyOwner {
