91e32ad8a34971f8ee6d03345f7fc5a37da71cdd
Will Doenlen <wdoenlen@gmail.com>
2017-10-02 22:05:30 -0700

    Send crowdsale proceeds to team wallet and charity wallet instead of single wallet

--- contracts/TetzelCrowdsale.sol
+++ contracts/TetzelCrowdsale.sol
@@ -5,15 +5,23 @@ import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 import './TetzelCoin.sol';
 
 contract TetzelCrowdsale is Crowdsale {
-  address public wallet = 0x244b236b19ea4cA308A994edd51A786C726B7864; // xcxc - Change this to real wallet
-  uint256 public rate = 500; // 1 eth = 500 TZL
+  address public teamWallet = 0x244b236b19ea4cA308A994edd51A786C726B7864; // xcxc - Change this to real wallet
+  address public charityWallet = 0x8b2448602f53608F86cf2c31D60eb3142a1596d4; // xcxc - Change this to real wallet
+  uint256 public rate = 500; // 1 eth = 500 SIN
   uint256 public startTime = block.timestamp;
-  uint256 public endTime = block.timestamp + 1;
+  uint256 public endTime = block.timestamp + 100;
 
-  function TetzelCrowdsale() Crowdsale(startTime, endTime, rate, wallet) {}
+  function TetzelCrowdsale() Crowdsale(startTime, endTime, rate, teamWallet) {}
 
   function createTokenContract() internal returns (MintableToken) {
     return new TetzelCoin();
+  }
+
+  function forwardFunds() internal {
+    uint256 teamValue = msg.value * 15 / 100; // Team keeps 15%
+    uint256 charityValue = msg.value - teamValue; // Charity keeps 85%
+    wallet.transfer(teamValue);
+    charityWallet.transfer(charityValue);
   }
 
 }
