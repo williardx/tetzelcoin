8d92471fabebb324e72c187ab56e75192a2c098b
Will Doenlen <wdoenlen@gmail.com>
2017-10-02 18:04:33 -0700

    Update TetzelCrowdsale to be compatible with new open zeppelin contract

--- contracts/TetzelCrowdsale.sol
+++ contracts/TetzelCrowdsale.sol
@@ -7,9 +7,10 @@ import './TetzelCoin.sol';
 contract TetzelCrowdsale is Crowdsale {
   address public wallet = 0x244b236b19ea4cA308A994edd51A786C726B7864; // xcxc - Change this to real wallet
   uint256 public rate = 500; // 1 eth = 500 TZL
-  uint256 public endBlock = 999999999; // xcxc - use real endblock
+  uint256 public startTime = block.timestamp;
+  uint256 public endTime = block.timestamp + 1;
 
-  function TetzelCrowdsale() Crowdsale(block.number, endBlock, rate, wallet) {}
+  function TetzelCrowdsale() Crowdsale(startTime, endTime, rate, wallet) {}
 
   function createTokenContract() internal returns (MintableToken) {
     return new TetzelCoin();
