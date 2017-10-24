a80a25cc8becc06491b5997df5631ea90305b99d
Will Doenlen <wdoenlen@gmail.com>
2017-10-24 14:48:33 -0700

    Remove commented out code in migrations script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,28 +1,38 @@
 const TetzelCoin = artifacts.require("./TetzelCoin.sol");
 const TetzelCrowdsale = artifacts.require("./TetzelCrowdsale.sol");
 const Tetzel = artifacts.require("./Tetzel.sol");
+const TetzelTeamWallet = artifacts.require("./TetzelTeamWallet.sol");
 
 // Data for TetzelCrowdsale
-const teamWallet = '0x244b236b19ea4cA308A994edd51A786C726B7864';
 const charityWallet = '0x8b2448602f53608F86cf2c31D60eb3142a1596d4';
 const teamPortion = 15;
 const totalTeamMemberAllocation = 15;
 const rate = 500;
 
+let tetzelCoinInstance, tetzelTeamWalletInstance, tetzelCrowdsaleInstance;
+
 module.exports = function(deployer) {
-  deployer.deploy(TetzelCoin).then(() => {
-    return deployer.deploy(
-      TetzelCrowdsale, 
-      TetzelCoin.address,
-      teamWallet,
-      charityWallet,
-      teamPortion,
-      0, // TODO: figure out better way to test contracts and pass in startTime / endTime
-      0,
-      rate,
-      totalTeamMemberAllocation
-    ).then(() => {
-      return deployer.deploy(Tetzel, TetzelCrowdsale.address);
+  deployer.deploy(TetzelCoin).then((instance) => {
+    return deployer.deploy(TetzelTeamWallet).then(() => {
+      return deployer.deploy(
+        TetzelCrowdsale, 
+        TetzelCoin.address,
+        TetzelTeamWallet.address,
+        charityWallet,
+        teamPortion,
+        0, // TODO: figure out better way to test contracts and pass in startTime / endTime
+        0,
+        rate,
+        totalTeamMemberAllocation
+      ).then(() => {
+
+        TetzelCoin.deployed().then((tetzelCoinInstance) => {
+          tetzelCoinInstance.addMinter(TetzelCrowdsale.address);
+        });
+
+        return deployer.deploy(Tetzel, TetzelCrowdsale.address);
+      
+      });
     });
   });
 };
